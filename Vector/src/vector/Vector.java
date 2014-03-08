6350e1304d8158c17122b2bb827f8e36082821c0
14 <14@I14>
2014-03-08 07:52:19 -0500

    new commit

---
+++ Vector/src/vector/Vector.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vector;
+
+import javax.swing.*;
+public class Vector {
+
+    
+    public static void main(String[] args) {
+        
+        	int vec[];
+    	vec=new int[5];
+    	int dato,i;
+
+        for (i=0; i<=4; i++){
+        	dato = Integer.parseInt(JOptionPane.showInputDialog(" Digite el dato de la posición"));
+        	if (dato>8){
+       				 vec[i]=dato;
+        		   	 System.out.print(dato + " ");
+    }else
+    	System.out.print( " Dato Menor " );
+    	
+        
+    }
+    
+  }
+}
